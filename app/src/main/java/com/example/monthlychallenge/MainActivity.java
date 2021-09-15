20b7ad5ba4ff3c5e4eb4f47d070f5bdebd2b4f8f
ekdud0529 <ekffkddl0529@naver.com>
2021-09-15 14:50:20 +0900

    modify all

--- app/src/main/java/com/example/monthlychallenge/MainActivity.java
+++ app/src/main/java/com/example/monthlychallenge/MainActivity.java
@@ -1,14 +1,21 @@
 package com.example.monthlychallenge;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
